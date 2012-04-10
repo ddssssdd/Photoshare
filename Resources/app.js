fbc9536280f8d07b41e3d696f3e8aadb0e244d0b
Steven Fu <fuxinyu@gmail.com>
2012-04-10 11:02:08 +0800

    fixed bugs.

--- Resources/app.js
+++ Resources/app.js
@@ -44,7 +44,7 @@ var LL = function(key) {
 
 
 var settings={showAnimation:false,
-	serverUrl:"http://beta.pinspire.com/ios/",
+	serverUrl:"http://www.pinspire.com/ios/",
 	defaultImageColor:'#eee',
 	defaultCountry:'usa',
 	timeOut:20000,
